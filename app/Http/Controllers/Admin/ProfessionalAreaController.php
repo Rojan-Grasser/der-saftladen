5c5016502dd4095937823bae73e5106922fac1d9
Rojan Grasser <rojan.jamie@gmail.com>
2026-01-21 13:11:47 +0100

    feat(#45): Added new route for editing a professional area

--- app/Http/Controllers/Admin/ProfessionalAreaController.php
+++ app/Http/Controllers/Admin/ProfessionalAreaController.php
@@ -13,7 +13,6 @@ class ProfessionalAreaController extends Controller
     /**
      * Takes name and description as parameters, returns success (always) and message (sometimes).
      *
-     * @param Request $request
      * @return array|false[]|true[]
      */
     public function index(Request $request)
@@ -38,6 +37,39 @@ class ProfessionalAreaController extends Controller
                 return [
                     'success' => false,
                     'message' => 'The professional area "'.$name.'" already exists',
+                ];
+            }
+
+            return [
+                'success' => false,
+            ];
+        }
+    }
+
+    public function edit(Request $request, string $id)
+    {
+        $validated = $request->validate([
+            'name' => ['sometimes', 'string'],
+            'description' => ['sometimes', 'string'],
+        ]);
+
+        $professionalArea = ProfessionalArea::find($id);
+
+        $updateData = [
+            'name' => array_key_exists('name', $validated) ? trim($validated['name']) : $professionalArea->name,
+            'description' => array_key_exists('description', $validated) ? trim($validated['description']) : $professionalArea->description,
+        ];
+
+        try {
+            $professionalArea->update($updateData);
+
+            return ['success' => true];
+        } catch (Exception $exception) {
+            // Error code 23000 -> Unique violation on the name
+            if ($exception instanceof QueryException && $exception->errorInfo[0] === '23000') {
+                return [
+                    'success' => false,
+                    'message' => 'The professional area "'.$updateData['name'].'" already exists',
                 ];
             }
 
