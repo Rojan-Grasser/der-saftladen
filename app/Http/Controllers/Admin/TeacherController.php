8ec7a83b35c8f8051b8c262469f759ae8240cd15
Rojan Grasser <rojan.jamie@gmail.com>
2026-01-23 08:26:20 +0100

    feat: Added new get teachers role

--- app/Http/Controllers/Admin/TeacherController.php
+++ app/Http/Controllers/Admin/TeacherController.php
@@ -15,14 +15,14 @@ class TeacherController extends Controller
      */
     public function index(Request $request)
     {
-        $name = trim($request->validate(['query' => ['nullable', 'string', 'max:255']])['query'] ?? '');
+        $queryString = trim($request->validate(['query' => ['nullable', 'string', 'max:255']])['query'] ?? '');
 
         $query = DB::table('users')
             ->select('id', 'email', 'name');
 
-        if ($name !== '') {
-            $query->where('name', 'like', '%' . $name . '%')
-                ->orWhere('email', 'like', '%' . $name . '%');
+        if ($queryString !== '') {
+            $query->where('name', 'like', '%' . $queryString . '%')
+                ->orWhere('email', 'like', '%' . $queryString . '%');
         }
 
         $query->where('role', '=', UserRole::TEACHER);
