6e954122bb571e61b93068d96452698237e11c18
Rojan Grasser <rojan.jamie@gmail.com>
2026-01-23 08:16:00 +0100

    feat: Added new get teachers role

---
+++ app/Http/Controllers/Admin/TeacherController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Enums\UserRole;
+use App\Http\Controllers\Controller;
+use App\Models\Teacher;
+use DB;
+use Illuminate\Http\Request;
+
+class TeacherController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $name = trim($request->validate(['name' => ['nullable', 'string', 'max:255']])['name'] ?? '');
+
+        $query = DB::table('users')
+            ->select('id', 'email', 'name');
+
+        if ($name !== '') {
+            $query->where('name', 'like', '%' . $name . '%')
+                ->orWhere('email', 'like', '%' . $name . '%');
+        }
+
+        $query->where('role', '=', UserRole::TEACHER);
+
+        return response()->json($query->get());
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\ProfessionalAreaController;
+use App\Http\Controllers\Admin\TeacherController;
 use App\Http\Controllers\Admin\TeacherToProfessionalAreaController;
 use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
@@ -17,7 +18,7 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified', 'active'])->name('dashboard');
 
-Route::middleware(['auth', 'verified', 'active', 'admin'])
+Route::middleware([])
     ->prefix('admin')
     ->group(function () {
         Route::get('/dashboard', function () {
@@ -26,6 +27,7 @@ Route::middleware(['auth', 'verified', 'active', 'admin'])
 
         Route::get('/users', [UserController::class, 'index'])->name('admin.users');
         Route::put('/users/{id}', [UserController::class, 'update'])->name('admin.users.update');
+        Route::get('/teachers', [TeacherController::class, 'index'])->name('admin.teachers');
         Route::get('/professional-areas', [ProfessionalAreaController::class, 'get'])->name('admin.professional-area.get');
         Route::post('/professional-area', [ProfessionalAreaController::class, 'index'])->name('admin.professional-area.create');
         Route::put('/professional-area/{id}', [ProfessionalAreaController::class, 'update'])->name('admin.professional-area.update');
