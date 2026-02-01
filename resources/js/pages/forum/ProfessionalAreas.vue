f1da030bc3c51170501a6b5a5ae11611fcd54ab5
Rojan Grasser <rojan.jamie@gmail.com>
2026-02-01 22:10:06 +0100

    fix(#96): Fixed migrations

--- resources/js/pages/forum/ProfessionalAreas.vue
+++ resources/js/pages/forum/ProfessionalAreas.vue
@@ -3,6 +3,7 @@ import { Head } from '@inertiajs/vue3';
 
 import AppLayout from '@/layouts/AppLayout.vue';
 import AreaMapper from '@/pages/forum/components/AreaMapper.vue';
+import NoAreasFound from '@/pages/forum/components/NoAreasFound.vue';
 import { ProfessionalArea } from '@/pages/forum/types';
 import { areas as areasShow } from '@/routes/forum';
 import { BreadcrumbItem } from '@/types';
@@ -21,8 +22,15 @@ const breadcrumbs: BreadcrumbItem[] = [
     <Head title="Berufsbereiche" />
 
     <AppLayout :breadcrumbs="breadcrumbs">
-        <div class="px-10 pt-4">
+        <div v-if="areas.length" class="px-10 pt-4">
             <AreaMapper :areas="areas" />
+        </div>
+
+        <div
+            v-if="!areas.length"
+            class="flex min-h-[calc(100vh-12rem)] px-10 pt-4"
+        >
+            <NoAreasFound />
         </div>
     </AppLayout>
 </template>
